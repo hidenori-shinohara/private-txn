4f1035453ddb9c98ec4bbeb799e1b59725ce9701
Hidenori Shinohara <hidenori@stellar.org>
2023-01-17 15:04:03 -0500

    okay, i think it works now

--- private-txn.circom
+++ private-txn.circom
@@ -2,10 +2,14 @@ pragma circom 2.0.0;
 
 function commit(m, r) {
     // A very silly commit function
-    return m + r;
+    // I have no idea why,
+    // but it seems important that we multiple 1.
+    // It might have something to do with quadratic
+    // vs linear polynomials.
+    return 1 * m + r;
 }
 
-template Multiplier2() {
+template PrivateTxn() {
     signal input m1;
     signal input r1;
     signal input m2;
@@ -18,49 +22,26 @@ template Multiplier2() {
     signal output c3;
     c1 <== commit(m1, r1);
     c2 <== commit(m2, r2);
-    c3 <== r1*r2 + r3 + fee;
+
+    // I have absolutely no idea
+    // but if I change this to commit(m3, r3)
+    // it doesn't compile... ????
+    c3 <== commit(m3, r3);
+
+    // * A has m1 dollars.
+    // * A sends m2 dollars to B.
+    // * A pays `fee` dollars in fees.
+    // * A's remaining balance is m3 dollars.
+    m1 === m2 + m3 + fee;
+
+
+    // Make sure that m2 >= 0 && m3 >= 0
+    signal sign2;
+    sign2 <-- m2 >= 0 ? 1 : 0;
+    signal sign3;
+    sign3 <-- m3 >= 0 ? 1 : 0;
+    (sign2 * sign3) === 1;
  }
 
- component main = Multiplier2();
+ component main = PrivateTxn();
 
-
-// pragma circom 2.0.0;
-// 
-// function commit(m, r) {
-//     // A very silly commit function
-//     return m + r;
-// }
-// 
-// function nonneg(m) {
-//     if (m >= 0)
-//     {
-//         return 1;
-//     }
-//     else
-//     {
-//         return 0;
-//     }
-// }
-// 
-// template PrivateTxn() {
-//     signal input m1;
-//     signal input r1;
-//     signal input m2;
-//     signal input r2;
-//     signal input m3;
-//     signal input r3;
-//     signal input fee;
-//     signal output c1;
-//     signal output c2;
-//     signal output c3;
-//     c1 <== commit(m1, r1);
-//     //c2 <== commit(m2, r2);
-//     //c3 <== commit(m3, r3);
-//     //m1 === m2 + m3 + fee;
-//     // I have no idea, but this doesn't compile.
-//     // I'll ignore this for now and fix it later.
-//     // sign2 = nonneg(m2);
-//     // sign3 = nonneg(m3);
-// }
-// 
-// component main = PrivateTxn();
--- private-txn_cpp/private-txn.cpp
+++ private-txn_cpp/private-txn.cpp
@@ -3,26 +3,26 @@
 #include <assert.h>
 #include "circom.hpp"
 #include "calcwit.hpp"
-void Multiplier2_0_create(uint soffset,uint coffset,Circom_CalcWit* ctx,std::string componentName,uint componentFather);
-void Multiplier2_0_run(uint ctx_index,Circom_CalcWit* ctx);
+void PrivateTxn_0_create(uint soffset,uint coffset,Circom_CalcWit* ctx,std::string componentName,uint componentFather);
+void PrivateTxn_0_run(uint ctx_index,Circom_CalcWit* ctx);
 void commit_0(Circom_CalcWit* ctx,FrElement* lvar,uint componentFather,FrElement* destination,int destination_size);
 Circom_TemplateFunction _functionTable[1] = { 
-Multiplier2_0_run };
+PrivateTxn_0_run };
 Circom_TemplateFunction _functionTableParallel[1] = { 
 NULL };
 uint get_main_input_signal_start() {return 4;}
 
 uint get_main_input_signal_no() {return 7;}
 
-uint get_total_signal_no() {return 11;}
+uint get_total_signal_no() {return 13;}
 
 uint get_number_of_components() {return 1;}
 
 uint get_size_of_input_hashmap() {return 256;}
 
-uint get_size_of_witness() {return 8;}
+uint get_size_of_witness() {return 6;}
 
-uint get_size_of_constants() {return 0;}
+uint get_size_of_constants() {return 2;}
 
 uint get_size_of_io_map() {return 0;}
 
@@ -51,19 +51,20 @@ delete ctx->componentMemory[pos].sbct;
 // function declarations
 void commit_0(Circom_CalcWit* ctx,FrElement* lvar,uint componentFather,FrElement* destination,int destination_size){
 FrElement* circuitConstants = ctx->circuitConstants;
-FrElement expaux[3];
+FrElement expaux[4];
 std::string myTemplateName = "commit";
 u64 myId = componentFather;
 // return bucket
-Fr_add(&expaux[0],&lvar[0],&lvar[1]); // line circom 5
+Fr_mul(&expaux[1],&circuitConstants[1],&lvar[0]); // line circom 9
+Fr_add(&expaux[0],&expaux[1],&lvar[1]); // line circom 9
 Fr_copy(destination,&expaux[0]);
 return;
 }
 
 // template declarations
-void Multiplier2_0_create(uint soffset,uint coffset,Circom_CalcWit* ctx,std::string componentName,uint componentFather){
+void PrivateTxn_0_create(uint soffset,uint coffset,Circom_CalcWit* ctx,std::string componentName,uint componentFather){
 ctx->componentMemory[coffset].templateId = 0;
-ctx->componentMemory[coffset].templateName = "Multiplier2";
+ctx->componentMemory[coffset].templateName = "PrivateTxn";
 ctx->componentMemory[coffset].signalStart = soffset;
 ctx->componentMemory[coffset].inputCounter = 7;
 ctx->componentMemory[coffset].componentName = componentName;
@@ -71,7 +72,7 @@ ctx->componentMemory[coffset].idFather = componentFather;
 ctx->componentMemory[coffset].subcomponents = new uint[0];
 }
 
-void Multiplier2_0_run(uint ctx_index,Circom_CalcWit* ctx){
+void PrivateTxn_0_run(uint ctx_index,Circom_CalcWit* ctx){
 FrElement* signalValues = ctx->signalValues;
 u64 mySignalStart = ctx->componentMemory[ctx_index].signalStart;
 std::string myTemplateName = ctx->componentMemory[ctx_index].templateName;
@@ -82,7 +83,7 @@ u32* mySubcomponents = ctx->componentMemory[ctx_index].subcomponents;
 bool* mySubcomponentsParallel = ctx->componentMemory[ctx_index].subcomponentsParallel;
 FrElement* circuitConstants = ctx->circuitConstants;
 std::string* listOfTemplateMessages = ctx->listOfTemplateMessages;
-FrElement expaux[5];
+FrElement expaux[6];
 FrElement lvar[0];
 uint sub_component_aux;
 uint index_multiple_eq;
@@ -115,14 +116,60 @@ commit_0(ctx,lvarcall,myId,&signalValues[mySignalStart + 1],1);
 }
 
 {
-PFrElement aux_dest = &signalValues[mySignalStart + 2];
-// load src
-Fr_mul(&expaux[2],&signalValues[mySignalStart + 4],&signalValues[mySignalStart + 6]); // line circom 21
-Fr_add(&expaux[1],&expaux[2],&signalValues[mySignalStart + 8]); // line circom 21
-Fr_add(&expaux[0],&expaux[1],&signalValues[mySignalStart + 9]); // line circom 21
-// end load src
-Fr_copy(aux_dest,&expaux[0]);
+
+// start of call bucket
+FrElement lvarcall[2];
+// copying argument 0
+Fr_copy(&lvarcall[0],&signalValues[mySignalStart + 7]);
+// end copying argument 0
+// copying argument 1
+Fr_copy(&lvarcall[1],&signalValues[mySignalStart + 8]);
+// end copying argument 1
+commit_0(ctx,lvarcall,myId,&signalValues[mySignalStart + 2],1);
+// end call bucket
 }
+
+Fr_add(&expaux[3],&signalValues[mySignalStart + 5],&signalValues[mySignalStart + 7]); // line circom 35
+Fr_add(&expaux[2],&expaux[3],&signalValues[mySignalStart + 9]); // line circom 35
+Fr_eq(&expaux[0],&signalValues[mySignalStart + 3],&expaux[2]); // line circom 35
+if (!Fr_isTrue(&expaux[0])) std::cout << "Failed assert in template/function " << myTemplateName << " line 35. " <<  "Followed trace of components: " << ctx->getTrace(myId) << std::endl;
+assert(Fr_isTrue(&expaux[0]));
+Fr_geq(&expaux[0],&signalValues[mySignalStart + 5],&circuitConstants[0]); // line circom 40
+if(Fr_isTrue(&expaux[0])){
+{
+PFrElement aux_dest = &signalValues[mySignalStart + 10];
+// load src
+// end load src
+Fr_copy(aux_dest,&circuitConstants[1]);
+}
+}else{
+{
+PFrElement aux_dest = &signalValues[mySignalStart + 10];
+// load src
+// end load src
+Fr_copy(aux_dest,&circuitConstants[0]);
+}
+}
+Fr_geq(&expaux[0],&signalValues[mySignalStart + 7],&circuitConstants[0]); // line circom 42
+if(Fr_isTrue(&expaux[0])){
+{
+PFrElement aux_dest = &signalValues[mySignalStart + 11];
+// load src
+// end load src
+Fr_copy(aux_dest,&circuitConstants[1]);
+}
+}else{
+{
+PFrElement aux_dest = &signalValues[mySignalStart + 11];
+// load src
+// end load src
+Fr_copy(aux_dest,&circuitConstants[0]);
+}
+}
+Fr_mul(&expaux[1],&signalValues[mySignalStart + 10],&signalValues[mySignalStart + 11]); // line circom 43
+Fr_eq(&expaux[0],&expaux[1],&circuitConstants[1]); // line circom 43
+if (!Fr_isTrue(&expaux[0])) std::cout << "Failed assert in template/function " << myTemplateName << " line 43. " <<  "Followed trace of components: " << ctx->getTrace(myId) << std::endl;
+assert(Fr_isTrue(&expaux[0]));
 for (uint i = 0; i < 0; i++){
 uint index_subc = ctx->componentMemory[ctx_index].subcomponents[i];
 if (index_subc != 0)release_memory_component(ctx,index_subc);
@@ -130,7 +177,7 @@ if (index_subc != 0)release_memory_component(ctx,index_subc);
 }
 
 void run(Circom_CalcWit* ctx){
-Multiplier2_0_create(1,0,ctx,"main",0);
-Multiplier2_0_run(0,ctx);
+PrivateTxn_0_create(1,0,ctx,"main",0);
+PrivateTxn_0_run(0,ctx);
 }
 
